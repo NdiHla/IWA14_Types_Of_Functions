dfc6d6375e80c4282a553d5c0a47294b07101a1c
Ndimande <mkatroso.deep@gmail.com>
2023-04-16 20:06:08 +0200

    Types_Of_Functions

---
+++ Scripts.js
@@ -0,0 +1,58 @@
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+const logTwice = () => {
+  console(parameter)
+  console(parameter)
+}
+
+function hobby () {
+  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+}
+
+function hobby () {
+    logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+  }
+
+2
+
+// script.js
+
+function add(){  a, b => { 48 + 12 }}
+
+function multiply() { a, b => { 48 - 12 }}
+
+function internal() {
+	const added = this.add(this.a, this.b)
+	this.multiply(this.a, this.b)
+	return this
+}
+
+// Not allowed to change below this
+
+const example1 = {
+	internal: {
+		a: 2,
+		b: 4,
+		c: 8,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+const example2 = {
+	internal: {
+		a: 2,
+		b: 2,
+		c: 3,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+example1.calculate()
+example2.calculate()
+ 
